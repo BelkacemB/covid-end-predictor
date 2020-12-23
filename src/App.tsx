9cad7e30c7d67b2bb62da88b0891782bf19a6cab
Belkacem BERCHICHE <b_berchiche@esi.dz>
2020-12-23 16:55:27 +0100

    First version of Covid-19 end date predictor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { getEndDate, getNumberOfVaccinationsPerDay } from "./model/Model";
+import "./App.css";
 
 function App() {
+  const [daysPeriod, setDaysPeriod] = useState(10);
+  const [threshold, setThreshold] = useState(70);
+  const [daysToGo, setDaysToGo] = useState(0)
+
+  useEffect(() => {
+    let vaccinationRate : number = getNumberOfVaccinationsPerDay(daysPeriod)
+    setDaysToGo(getEndDate(vaccinationRate, threshold));
+  }, [daysPeriod, threshold]);
+
+  const handleDaysChange = (event: any) => {
+    setDaysPeriod(event.target.value);
+  };
+
+  const handleThresholdChange = (event: any) => {
+    setThreshold(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>End of Covid-19 predictor</h2>
+      <p>
+        Based on the speed of vaccination in the last &nbsp;
+        <Select
+          labelId="daysPeriodLabel"
+          id="daysPeriodId"
+          value={daysPeriod}
+          onChange={handleDaysChange}
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={20}>20</MenuItem>
+          <MenuItem value={30}>30</MenuItem>
+        </Select>
+        days ,
+        <Select
+          labelId="thresholdLabel"
+          id="thresholdId"
+          value={threshold}
+          onChange={handleThresholdChange}
+        >
+          <MenuItem value={0.6}>60%</MenuItem>
+          <MenuItem value={0.7}>70%</MenuItem>
+          <MenuItem value={0.8}>80%</MenuItem>
+        </Select>
+        &nbsp; of the world population will be vaccinated in:
+      </p>
+      <h1>{daysToGo} days</h1>
     </div>
   );
 }
