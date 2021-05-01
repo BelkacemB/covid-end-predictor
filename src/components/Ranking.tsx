609fb22da3251c53d3409d62009956cd309375a3
Belkacem BERCHICHE <b_berchiche@esi.dz>
2021-05-01 13:35:03 +0200

    Add radio buttons to ranking table

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,38 +1,57 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   getAvailableCountries,
   getNumberOfVaccinationsPerDayPerRegion,
   getEndDate,
   dateCompare,
 } from "../model/Model";
+import {
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@material-ui/core";
 
 function Ranking(props: any) {
   let { data } = props;
+  const [vaccType, setVaccType] = useState("full");
+  const [contextualData, setContextualData] = useState(data);
+  const handleVaccTypeChange = (e: any) => {
+    setVaccType(e.target.value);
+  };
 
   const dataRef = useRef(data);
 
-  dataRef.current = data.map((x: any) => {
-    return {
-      ...x,
-      total_vaccinations: x.total_vaccinations - x.people_fully_vaccinated,
-    };
-  });
+  useEffect(() => {
+    dataRef.current = data.map((x: any) => {
+      return {
+        ...x,
+        total_vaccinations:
+          vaccType === "full"
+            ? x.people_fully_vaccinated == null
+              ? NaN
+              : x.people_fully_vaccinated
+            : x.total_vaccinations - x.people_fully_vaccinated,
+      };
+    });
+    setContextualData(dataRef.current);
+  }, [vaccType, data]);
 
   let todaysDate = new Date();
-  let availableCountries = getAvailableCountries(dataRef.current);
+  let availableCountries = getAvailableCountries(contextualData);
 
   let countriesRanked = availableCountries
     ?.map((country) => {
       let dailyVaccPerRegion = getNumberOfVaccinationsPerDayPerRegion(
         2,
         country,
-        dataRef.current
+        contextualData
       );
       let endDate = getEndDate(
         dailyVaccPerRegion,
         0.6,
         country,
-        dataRef.current
+        contextualData
       );
       let countryAndEndDate = { country: country, date: endDate };
       return countryAndEndDate;
@@ -60,6 +79,26 @@ function Ranking(props: any) {
 
   return (
     <div id="ranking">
+      <FormControl component="fieldset">
+        <RadioGroup
+          row
+          aria-label="vacc-type"
+          name="vacc-type"
+          value={vaccType}
+          onChange={handleVaccTypeChange}
+        >
+          <FormControlLabel
+            value="primo"
+            control={<Radio />}
+            label="First injections"
+          />
+          <FormControlLabel
+            value="full"
+            control={<Radio />}
+            label="Full vaccinations"
+          />
+        </RadioGroup>
+      </FormControl>
       <table>
         <tr>
           <th>Country</th>
