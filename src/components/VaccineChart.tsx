2f3af792956bc89c2183aa488104bb6e137283a9
Belkacem Berchiche <b_berchiche@esi.dz>
2022-03-27 22:13:24 +0200

    Integrated d3. Charting WIP

--- src/components/VaccineChart.tsx
+++ src/components/VaccineChart.tsx
@@ -1,13 +1,11 @@
 import { formatDateAxis, getFirstDaysOfMonths } from "../model/ChartUtils";
-import { axisBottom, axisLeft } from 'd3-axis';
-import { line } from 'd3-shape';
-import { select } from 'd3-selection';
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 export default function VaccineChart(props: any) {
-  
+
   const data = props.data;
-  let chart = useRef(null);
+  let svgRef = useRef(null);
   let dataWithProjection = [...data];
 
   // What's the typeof date object?
@@ -39,27 +37,68 @@ export default function VaccineChart(props: any) {
     return e;
   });
 
-  const margin = { top: 20, right: 20, bottom: 20, left: 80 },
-  fullWidth = 800,
-  fullHeight = 300,
-  width = fullWidth - margin.left - margin.right,
-  height = fullHeight - margin.top - margin.bottom;
-
   let threshold = (props.threshold * 100) / props.regionPopulation;
 
+  const margin = { top: 20, right: 20, bottom: 20, left: 80 },
+    fullWidth = 800,
+    fullHeight = 300,
+    width = fullWidth - margin.left - margin.right,
+    height = fullHeight - margin.top - margin.bottom;
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current)
+      .attr("width", fullWidth)
+      .attr("height", fullHeight)
+      .style('background-color', '#f5f5f5')
+      .style('margin-top', '50');
+
+    const x = d3
+      .scaleTime()
+      .domain([
+        d3.min(dataWithProjection, (d: any) => d.date),
+        d3.max(dataWithProjection, (d: any) => d.date),
+      ])
+      .range([0, width]);
+
+    const y = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(dataWithProjection, (d: any) => d.projected_vaccinations),
+      ])
+      .range([height, 0]);
+
+    const xAxis = d3.axisBottom(x).ticks(5) //.tickFormat(formatDateAxis);
+    const yAxis = d3.axisLeft(y).tickFormat((d: any) => {
+      return d + " (" + (d * (100 / props.regionPopulation)).toFixed(1) + "%)";
+    });
+
+    const line = d3
+      .line()
+      .x((d: any) => x(d.date))
+      .y((d: any) => y(d.total_vaccinations_per_population))
+      .curve(d3.curveMonotoneX);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .call(yAxis)
+
+    svg
+      .append("path")
+      .data([dataWithProjection])
+      .attr("fill", "none")
+      .attr('d', line)
+      .attr("stroke", "black")
+  }, [dataWithProjection]);
+
   return (
-    <svg
-      ref={chart}
-      height="100%"
-      width="100%"
-      viewBox={`0 0 ${fullWidth} ${fullHeight}`}
-    >
-      <g transform={`translate(${margin.left},${margin.top})`}>
-        <g className="axis x" transform={`translate(0, ${height})`} />
-        <g className="axis y" />
-        <path className="line baseline" />
-        <path className="line actual" />
-      </g>
-    </svg>
+    <div>
+      <svg ref={svgRef}></svg>
+    </div>
+
   );
 }
